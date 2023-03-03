91e4a8ec5524ceb2468b807527ae5ad85e77b0f1
emilbra <emilantoni@hotmail.no>
2023-03-03 20:37:12 +0100

    added independent network security rules instead of nested with nsg

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,41 +9,53 @@ resource "azurerm_network_security_group" "default_nsg" {
   name                = "default_nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  security_rule = [
-    {
+}
+
+resource "azurerm_network_security_rule" "allow_inbound_2302_UDP" {
+      resource_group_name = azurerm_resource_group.rg.name
+      network_security_group_name = azurerm_network_security_group.default_nsg.name
       access                     = "Allow"
       description                = "Allow inbound 2302 UDP Default GamePort"
-      destination_address_prefix = "*"
+      destination_address_prefix = "VirtualNetwork"
       destination_port_range     = "*"
       direction                  = "Inbound"
       name                       = "Inbound 2302 UDP"
-      priority                   = 10
+      priority                   = 100
       protocol                   = "Udp"
       source_address_prefix      = "*"
-    },
-    {
+}
+
+resource "azurerm_network_security_rule" "allow_inbound_27016_UDP" {
+      resource_group_name = azurerm_resource_group.rg.name
+      network_security_group_name = azurerm_network_security_group.default_nsg.name
       access                     = "Allow"
       description                = "Allow inbound 27016 UDP Steam Query Port"
-      destination_address_prefix = "*"
+      destination_address_prefix = "VirtualNetwork"
       destination_port_range     = "*"
       direction                  = "Inbound"
       name                       = "Inbound 27016 UDP"
-      priority                   = 15
+      priority                   = 150
       protocol                   = "Udp"
       source_address_prefix      = "*"
-    },
-    {
+}
+
+resource "azurerm_network_security_rule" "allow_inbound_2312_TCP" {
+      resource_group_name = azurerm_resource_group.rg.name
+      network_security_group_name = azurerm_network_security_group.default_nsg.name
       access                     = "Allow"
       description                = "Allow inbound 2312 TCP DZSALauncher"
-      destination_address_prefix = "*"
+      destination_address_prefix = "VirtualNetwork"
       destination_port_range     = "*"
       direction                  = "Inbound"
       name                       = "Inbound 2312 TCP for DZSALauncher"
-      priority                   = 20
+      priority                   = 200
       protocol                   = "Tcp"
       source_address_prefix      = "*"
-    },
-    {
+}
+
+resource "azurerm_network_security_rule" "deny_default_all_inbound" {
+      resource_group_name = azurerm_resource_group.rg.name
+      network_security_group_name = azurerm_network_security_group.default_nsg.name
       access                     = "Deny"
       description                = "Deny Default All Inbound communication that does not match other rules"
       destination_address_prefix = "*"
@@ -53,8 +65,6 @@ resource "azurerm_network_security_group" "default_nsg" {
       priority                   = 999
       protocol                   = "Tcp"
       source_address_prefix      = "*"
-    }
-  ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg_subnet_assoc" {
